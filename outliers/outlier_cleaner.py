af8d59b42d2be9f012dea19529834a26c189fdd0
BHouwens <zhion360@gmail.com>
2015-06-08 13:57:33 +0200

    Outlier removal and more basic regression

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,21 @@ def outlierCleaner(predictions, ages, net_worths):
         return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error)
     """
-    
+
+    import heapq
+
     cleaned_data = []
+    complete_data = []
+    errors = []
 
-    ### your code goes here
+    for i in range(90):
+        complete_data.append( (ages[i], net_worths[i], abs(predictions[i]) - abs(net_worths[i])) )
+        errors.append(complete_data[i][2])
 
-    
+    good_ones = heapq.nsmallest(81, errors)
+
+    for entry in complete_data:
+        if entry[2] in good_ones:
+            cleaned_data.append(entry)
+
     return cleaned_data
-
--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -30,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,13 +38,10 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color from "b" to "r"
 ### to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression
 
-
-
-
-
-
-
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -65,6 +62,8 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
